b35cc5a42843aca6ea8bc477d8b201e13319359d
DavidMarchant <davidwjmarchant@gmail.com>
2019-04-29 15:34:13 +0100

    Add initial CLI module

---
+++ lib/flight-message/cli.rb
@@ -0,0 +1,52 @@
+# =============================================================================
+# Copyright (C) 2019-present Alces Flight Ltd.
+#
+# This file is part of Flight Message.
+#
+# This program and the accompanying materials are made available under
+# the terms of the Eclipse Public License 2.0 which is available at
+# <https://www.eclipse.org/legal/epl-2.0>, or alternative license
+# terms made available by Alces Flight Ltd - please direct inquiries
+# about licensing to licensing@alces-flight.com.
+#
+# Flight Message is distributed in the hope that it will be useful, but
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED INCLUDING, WITHOUT LIMITATION, ANY WARRANTIES OR CONDITIONS
+# OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY OR FITNESS FOR A
+# PARTICULAR PURPOSE. See the Eclipse Public License 2.0 for more
+# details.
+#
+# You should have received a copy of the Eclipse Public License 2.0
+# along with Flight Message. If not, see:
+#
+#  https://opensource.org/licenses/EPL-2.0
+#
+# For more information on Flight Message, please visit:
+# https://github.com/openflighthpc/flight-message
+# ==============================================================================
+require 'commander'
+
+module FlightMessage
+  module CLI
+    PROGRAM_NAME = ENV.fetch('FLIGHT_PROGRAM_NAME','message')
+
+    extend Commander::Delegates
+    program :application, "Flight Message"
+    program :name, PROGRAM_NAME
+    program :version, "Release 2019.1 (v#{FlightMessage::VERSION})"
+    program :description, 'Asset notification handling system'
+    program :help_paging, false
+    default_command :help
+    silent_trace!
+
+    class << self
+      def cli_syntax(command, args_str = nil)
+        command.syntax = [
+          PROGRAM_NAME,
+          command.name,
+          args_str
+        ].compact.join(' ')
+       end
+    end
+  end
+end
